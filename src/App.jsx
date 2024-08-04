3e7404a6ed6a48c20abdf278b26851fdb0efc36e
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-08-04 12:40:09 +0300

    redux-persist

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,7 @@ import { ContactForm } from "./components/ContactForm/ContactForm";
 import { SearchBox } from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
-// const initialContacts = JSON.parse(localStorage.getItem("contacts")) || [
-//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-// ];
-
 function App() {
-  // useEffect(() => {
-  //   localStorage.setItem("contacts", JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <>
       <h1 className="title">Phonebook</h1>
